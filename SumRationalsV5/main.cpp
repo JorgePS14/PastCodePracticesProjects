cc69f985c7f8d1f22aff964b256805f130899f50
Jorge Pietra Santa <31036932+JorgePS14@users.noreply.github.com>
2018-09-12 01:49:21 -0500

    Add files via upload

---
+++ SumRationalsV5/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void sumRationals(const int *const a, const int &b, const int &c, const int &d, int *const e, int *const f);
+void printSumRational(const int *const a, const int &b, const int &c, const int &d, const int &e, const int &f);
+
+int main() {
+	int *a, b, c, d, e, f;
+
+	a = new int;
+	*a = 1;
+	b = 2;
+	c = 3;
+	d = 4;
+	e = 0;
+	f = 0;
+
+	sumRationals(a, b, c, d, &e, &f);
+	printSumRational(a, b, c, d, e, f);
+	delete a;
+	return 0;
+}
+
+void sumRationals(const int *const a, const int &b, const int &c, const int &d, int *const e, int *const f) {
+	*e = (a*b + b * c) / (b*d);
+	*f = b * d;
+	a = 100;
+}
+
+void printSumRational(const int *const a, const int &b, const int &c, const int &d, const int &e, const int &f) {
+	cout << a << "*/" << b << "+" << c << "/" << d << " = " << e << "/" << f << endl;
+}
